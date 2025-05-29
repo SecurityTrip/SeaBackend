0872996e484d63d576a4eb45bb0f28ffb5c16ea9
ilia <illigiorno@gmail.com>
2025-05-29 12:20:35 +0400

    fix?

--- src/main/java/ru/securitytrip/backend/config/WebSocketConfig.java
+++ src/main/java/ru/securitytrip/backend/config/WebSocketConfig.java
@@ -18,8 +18,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000", "http://localhost") // разрешить нужные origin
-                .withSockJS();
+                .setAllowedOrigins("*") // разрешить нужные origin
+                .withSockJS()
+                .setSessionCookieNeeded(false);
     }
 
     @Override
